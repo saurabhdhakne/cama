df63c8c65f997b605d783a7b0d12ff73ea6366d8
Saurabh Dhakne <53269199+saurabhdhakne@users.noreply.github.com>
2022-07-09 14:44:15 +0530

    update password

--- admin/check_con.php
+++ admin/check_con.php
@@ -2,7 +2,7 @@
 
 $servername = 'localhost';
 $username = 'root';
-$password = '';
+$password = 'CodeAppMedia@9860';
 $dbname = 'codeappmediaar';
 
 $conn = mysqli_connect($servername, $username, $password, $dbname);
